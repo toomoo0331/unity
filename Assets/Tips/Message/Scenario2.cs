4c04a1924014195a47315fbe6bfe2c984454c1c8
toomoo0331 <33276284+toomoo0331@users.noreply.github.com>
2019-12-26 15:19:41 +0900

    firstcommit

---
+++ Assets/Tips/Message/Scenario2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario2 : MonoBehaviour {
+
+    public MessageController2 msgctrl;
+    public MessageController2 msgctrl_telop;
+
+    // Use this for initialization
+    void Start()
+    {
+        msgctrl.SetMessage(
+            "空　こぼれ落ちたふたつの星が\n" +
+            "光と闇の水面　吸い込まれてゆく\n" +
+            "引き合うように　重なる波紋\n"
+            );
+        
+        msgctrl_telop.SetMessage("そのとき！！");
+    }
+}
